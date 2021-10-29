eeba573d5c3a179ffdf4358246b9acebc878cd9f
Mnigos <makowskiigor@gmail.com>
2021-10-29 13:24:29 +0200

    feat: create toClean function

---
+++ src/toClean.ts
@@ -0,0 +1,52 @@
+import { ToCleanConfig } from './interfaces/ToCleanConfig'
+import { regexpEscape } from './utils/regexpEscape'
+
+const defaultConfig: ToCleanConfig = {
+  decimalMark: '.',
+  thousandSeparator: ',',
+  precision: [2, 2],
+}
+
+export function toClean(
+  value: number,
+  {
+    decimalMark = '.',
+    thousandSeparator = ',',
+    precision = [2, 2],
+  }: ToCleanConfig = defaultConfig
+) {
+  if (precision.length !== 2)
+    throw new Error('`precision` property must got two elements')
+
+  if (Number.isNaN(value)) return 'NaN'
+
+  const fixedValue = value.toFixed(precision[1])
+  const dotIndex = fixedValue.lastIndexOf('.')
+
+  let valueWithDotIndex =
+    precision[0] > 0
+      ? fixedValue.slice(0, dotIndex) +
+        decimalMark +
+        fixedValue.slice(dotIndex + 1)
+      : fixedValue + decimalMark
+
+  if (precision[0] > 0) {
+    const numZeros =
+      dotIndex > -1
+        ? precision[0] - (valueWithDotIndex.length - dotIndex - 1)
+        : precision[0]
+
+    for (let i = 0; i < numZeros; i++) valueWithDotIndex += '0'
+
+    const regexpDecimalMark = regexpEscape(decimalMark)
+    const thousandSeparatorRegexp = new RegExp(
+      `\\d(?=(\\d{3})+${regexpDecimalMark})`,
+      'g'
+    )
+    const trimRegexp = new RegExp(`${regexpDecimalMark}$`)
+
+    return valueWithDotIndex
+      .replace(thousandSeparatorRegexp, `$&${thousandSeparator}`)
+      .replace(trimRegexp, '')
+  }
+}
